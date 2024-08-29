22c04188238da05b7387aca09a08a382bf79af42
shreya <89719271+ShreyaPSawant@users.noreply.github.com>
2024-08-29 14:32:54 +0530

    leaves module done

--- leaves.py
+++ leaves.py
@@ -1,11 +1,33 @@
+from datetime import date
+import os
+import csv
+
 class Leave:
-    def __init__(self, leave_id: int, start_date: date, end_date: date, leave_type: str, status: str):
-        self.leave_id = leave_id
+    def apply_leave(self, employee_id, start_date, end_date, available_leaves):
+        self.employee_id = employee_id
         self.start_date = start_date
         self.end_date = end_date
-        self.type = leave_type
-        self.status = status
+        self.applied_date = date.today()
+        self.available_leaves= available_leaves-1
+        leave_details = [employee_id, self.start_date, self.end_date,self.applied_date,self.available_leaves]
+        self.write_to_csv('employee_leaves_data.csv', leave_details)
+        
+    def write_to_csv(self, filename, leave_details):
+        file_exists = os.path.isfile(filename)
+        
+        with open(filename, mode='a') as file:
+            writer = csv.writer(file)
+            
+            if not file_exists:
+                writer.writerow(['Employee ID', 'Start Date', 'End Date','Applied on', 'Available leaves'])
+            
+            writer.writerow(leave_details)
 
-    def apply_leave(self):
-        # Logic to apply for a leave
-        pass
+    
+
+    def show_leaves_left(self,available_leaves):
+          print("leaves left for employee id:",self.employee_id," are =",available_leaves)
+        
+
+employee_leave=Leave()
+employee_leave.apply_leave(104,'2000-09-02','2000-09-02',6)
