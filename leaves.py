58db68a9dd1e4c4d094335b36da70fe0d42a2774
shreya <89719271+ShreyaPSawant@users.noreply.github.com>
2024-08-30 15:26:13 +0530

    leaves final

--- leaves.py
+++ leaves.py
@@ -3,23 +3,25 @@ import os
 import csv
 # from file_handling import read_csv
 from file_handling import FileHandling
-leaves_data_filename='employee_leaves_data.csv'
+from employee_manager import EmployeeManager
+leaves_data_filename='employee_leave_data.csv'
 employee_data_filename='employee_data.csv'
 
 class Leave:
 
-    def take_leave(self, employee_id, start_date, end_date, available_leaves):
-        try:
-            employee_data=FileHandling.read_csv(employee_data_filename)
-            print(employee_data)
-            leaves_data=FileHandling.read_csv(leaves_data_filename)
-            for row in leaves_data:
+    def take_leave(self, employee_id, start_date, end_date):
+        
 
-                pass
-            for row in employee_data:
-                if int(row['employee_id'])==employee_id:
-                    available_leaves = available_leaves - 1
-                    FileHandling.save_info_employee()
+        try:
+            available_leaves_str = EmployeeManager().get_employee_available_leaves(employee_id)
+            applied_date = datetime.today()
+            available_leaves = int(available_leaves_str) - 1
+            leave_details = [employee_id, start_date, end_date,applied_date,available_leaves]
+            FileHandling().save_leave_info(leave_details)
+            
+            
+            EmployeeManager().set_employee_available_leaves(employee_id, available_leaves)
+
 
             
         except Exception as e:
@@ -27,17 +29,19 @@ class Leave:
 
     
 
-    def show_leaves_left(self, employee_id, available_leaves):
+    def show_leaves_left(self, employee_id):
         try:
             employee_data=FileHandling.read_csv(employee_data_filename)
             for row in employee_data:
                 if int(row['employee_id'])==employee_id:
-                    print("Leaves left for employee id:", employee_id, " are =", available_leaves)
+                    employee_leaves=EmployeeManager.get_employee_available_leaves(employee_id)
+                    print("Leaves left for employee id:", employee_id, " are =", employee_leaves)
         except Exception as e:
             print(f"An error occurred while showing leaves left: {e}")
 
     
 
 leave_manager = Leave()
-leave_manager.apply_leave(101, '2024-09-01', '2024-09-05', 5)
-leave_manager.show_leaves_left()
+leave_manager.take_leave(124, '2024-09-01', '2024-09-05')
+# leave_manager.show_leaves_left(106)
+# print(EmployeeManager().get_employee_available_leaves(124))
